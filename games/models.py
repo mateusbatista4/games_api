f5bf6c905e584056f4d123b081b37477867963e5
Mateus Batista <mateus999batista@gmail.com>
2020-04-16 23:04:51 -0300

    algmas mudanças

--- games/models.py
+++ games/models.py
@@ -1,14 +1,67 @@
 from django.db import models
 
-# Create your models here.
+#
+class EsrbRating(models.Model):
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('description',)
+
+    def __str__(self):
+        return self.description
+
+
+
+
 class Game(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200,default='')
     release_date = models.DateTimeField()
-    esrb_rating = models.CharField(max_length=150,default='')
+    esrb_rating = models.ForeignKey(
+        EsrbRating,
+        related_name='games',
+        on_delete=models.CASCADE
+    )
     played_once = models.BooleanField(default=False)
     played_times = models.IntegerField(default=0)
 
     class Meta:
         ordering = ('name',)
 
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+        (MALE,'Fale'),
+        (FEMALE,'Female')
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(
+        max_length=50,
+        choices=GENDER_CHOICES,
+        default= MALE
+    )
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class PlayerScore(models.Model):
+    player = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name= 'scores'
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE
+    )
+    score = models.IntegerField()
+    score_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-score',)
